9a6d53e4eb3595781a1f1523e182a1fabed37966
Ben Airey <benjaminjohnairey@gmail.com>
2024-12-05 20:15:58 -0600

    adding prompt toolkit history

--- bish/main.py
+++ bish/main.py
@@ -2,10 +2,13 @@
 
 import inspect
 import os
+import pathlib
 import platform
 import re
 
 import colorama
+from prompt_toolkit import PromptSession
+from prompt_toolkit.history import InMemoryHistory
 
 from bish import builtins
 from bish.errors import CommandNotFound
@@ -32,11 +35,33 @@ def load_builtins():
             BUILTINS[cmd.name] = cmd
 
 
+def get_histfile() -> pathlib.Path:
+    """Returns the path to the history file."""
+    if histfile := ENV_VARS.get("HISTFILE"):
+        if isinstance(histfile, Path):
+            return histfile.value.expanduser().resolve()
+    # Else, return a default value
+    return pathlib.Path("~/.turtle_history").expanduser().resolve()
+
+
 def main():
     print("bish version " + VERSION)
     if platform.system() not in ("Windows", "Linux", "Darwin"):
         print(f"Unsupported platform: '{platform.system()}'. Must exit now.")
         exit(1)
+
+
+    # Initialize prompt history
+    histfile = get_histfile()
+    with histfile.open("r") as f:
+        history = [line.strip() for line in f.readlines()]
+    # Only keep the last 100 commands
+    if len(history) > 100:
+        history = history[-100:]
+        with histfile.open("w") as f:
+            f.write(os.linesep.join(history))
+    
+    prompt_session = PromptSession(history=InMemoryHistory(history_strings=history))
 
     while True:
         prompt1: str = ENV_VARS["PROMPT1"]
@@ -44,18 +69,16 @@ def main():
         for env_var in env_vars:
             prompt1 = prompt1.replace(env_var, str(ENV_VARS.get(env_var[1:], "")))
 
-        input_ = [input(prompt1).strip()]
+        input_ = [prompt_session.prompt(prompt1).strip()]
         while not is_complete(" ".join(input_)):
-            input_.append(input(ENV_VARS["PROMPT2"]))
+            input_.append(prompt_session.prompt(ENV_VARS["PROMPT2"]))
 
         input_ = concatenate_incomplete_lines(input_)
 
         # Log input to history file
-        print(ENV_VARS.get("HISTFILE"))
-        if histfile := ENV_VARS.get("HISTFILE"):
-            if isinstance(histfile, Path):
-                with histfile.value.expanduser().resolve().open("a") as f:
-                    f.write(input_ + os.linesep)
+        histfile = get_histfile()
+        with histfile.open("a") as f:
+            f.write(input_ + os.linesep)
 
         # If nothing is entered, just move to next loop
         if not input_:
